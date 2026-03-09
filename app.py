ef8d4fdb203810d1adfe3133aca57865a27d2d73
Romshtex <vybornov.roman@gmail.com>
2026-03-09 09:03:35 +0300

    Update cache_resource decorator in app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --- ИНИЦИАЛИЗАЦИЯ COOKIES ---
-@st.cache_resource(experimental_allow_widgets=True)
+@st.cache_resource
 def get_manager():
     return stx.CookieManager()
 cookie_manager = get_manager()
